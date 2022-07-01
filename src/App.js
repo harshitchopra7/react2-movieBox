8e326306280f237f9da96b866435c3411efcd7a9
harshitchopra7 <chopraharshit2000@gmail.com>
2022-07-01 21:27:26 +0530

    tmdb api integrated

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./container/LandingPage/LandingPage";
 import SignInPage from "./container/SignInPage/SignInPage";
+import HomePage from "./container/HomePage/HomePage";
 
 function App() {
 
-  const [user, setUser] = useState(false)
+  const [user, setUser] = useState(true)
 
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={user ? <p>User logged in</p> : <LandingPage />} />
+          <Route path="/" element={user ? <HomePage /> : <LandingPage />} />
           <Route path="/signin-signup" element={<SignInPage />} />
         </Routes>
       </div>
