0b9a4d137ffd544e655566425b595558000a673b
harshitchopra7 <chopraharshit2000@gmail.com>
2022-07-02 13:55:53 +0530

    api integration revision and movies row created

--- src/container/HomePage/HomePage.js
+++ src/container/HomePage/HomePage.js
@@ -1,34 +1,38 @@
-import { getValue } from '@testing-library/user-event/dist/utils'
-import React, { useEffect, useState } from 'react'
-import "./HomePage.css"
+import { getValue } from "@testing-library/user-event/dist/utils";
+import React, { useEffect, useState } from "react";
+import "./HomePage.css";
 
 function HomePage() {
+  // const API_KEY = "82372dfcea1ebf3b69159ee3abff1e56"
 
-    const [topRatedMovies, setTopRatedMovies] = useState([])
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
 
-    // const API_ENDPOINT = "https://api.themoviedb.org/3" 
-    // const API_KEY = "82372dfcea1ebf3b69159ee3abff1e56"
+  // APIs
+  // Top Rated Movies
+  // "https://api.themoviedb.org/3/movie/top_rated?api_key=82372dfcea1ebf3b69159ee3abff1e56"
 
-    // APIs
-    // Top Rated Movies
-    // https://api.themoviedb.org/3/movie/top_rated?api_key=82372dfcea1ebf3b69159ee3abff1e56
-
-    // `${API_ENDPOINT}/movie/top_rated?api_key=${API_KEY}`
-
-    useEffect(() => {
-       fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=82372dfcea1ebf3b69159ee3abff1e56")
-       .then(data => data.json())
-       .then(res => setTopRatedMovies(res.results))
-       .catch(err => console.log(err))
-    }, [])
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/top_rated?api_key=82372dfcea1ebf3b69159ee3abff1e56"
+    )
+      .then((res) => res.json())
+      .then((data) => setTopRatedMovies(data.results))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <div>
-        {topRatedMovies.map((val, key) => (
-            <p>{val.title}</p>
+      <div className="movies_row_wrapper">
+        {topRatedMovies.map((value, key) => (
+          <div className="movies_row_container">
+            <img
+              src={`https://image.tmdb.org/t/p/original${value.poster_path}`}
+            />
+          </div>
         ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
